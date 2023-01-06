43ccf6a7501e9c751944bed140ddc97ccc181a26
Niklas Pirnay <n.pirnay@tu-berlin.de>
2023-01-06 03:08:32 +0100

    added transistion from any symbol

--- dfa.py
+++ dfa.py
@@ -7,8 +7,12 @@ class DFA:
         self.current_state = start_state
         self.transitions = {}
 
-    def add_transition(self, from_state, input_symbol, to_state):
+    def add_transition(self, from_state, to_state, input_symbol):
         self.transitions[(from_state, input_symbol)] = to_state
+
+    def add_transition_any_symbol(self, from_state, to_state):
+        for symbol in self.alphabet:
+            self.add_transition(from_state, to_state, symbol)
 
     def evaluate(self, input_string):
         self.current_state = self.start_state
